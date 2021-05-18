f19797ab7952f28844c6a1daccbc2f87474168dd
Luobo <luobo@wuzhiniu.com>
2021-05-18 18:04:18 +0800

    feat: 1.0.0 正式发布

--- README.md
+++ README.md
@@ -1,24 +1,50 @@
-# vue-simple-editor
+# 🍓 vue-simple-editor
+```
+基于 vue 3.x 轻量级富文本编辑器，可用于聊天框，发布框..
+```
+- ⭐ [gitHub](https://github.com/Denny-di/vue-simple-editor)
 
-## Project setup
+## Install
 ```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
+npm install vue-simple-editor
 ```
 
-### Lints and fixes files
+## Usage
 ```
-npm run lint
+import SimpleEditor from "vue-simple-editor";
+const app = createApp(App);
+app.use(SimpleEditor);
 ```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+## Props
+Name|Type|Default|Description
+--|:--:|--:|--:
+v-model:value|[Number, String]|-|获取富文本
+v-model:text|[Number, String]|-|获取纯文本
+
+## Events
+Name|Parameters|Description
+--|--:|--:
+insertNode|`type="text", content, value`|插入节点 `type: text（文本节点）、tag（标签节点），content：显示内容， value：获取纯文本的内容`
+getHtml|-|获取富文本
+getText|-|获取纯文本
+
+
+## Examples
+```
+<template>
+    <simple-editor></simple-editor>
+</template>
+<script>
+export default {
+    name: "App",
+    components: {},
+};
+</script>
+<style lang="less"></style>
+
+```
+
+![Examples](/packages/img/examples.jpg "Examples")
+
+
--- packages/index.vue
+++ packages/index.vue
@@ -1,79 +1,155 @@
 <template>
     <div class="simple-editor">
         <div class="editor-wrap">
-            <div class="btn-wrap">
-                <!-- <v-emoji @submit="add('emoji', $event)"></v-emoji> -->
-                <a-button @click="add('new')">@进群新人</a-button>
-            </div>
+            <slot>
+                <div class="btn-wrap">
+                    <button class="emoji-btn" @click="show_emoji = !show_emoji">
+                        表情
+                        <div class="emoji-warp" v-if="show_emoji">
+                            <v-emoji
+                                @submit="
+                                    insertNode('text', $event),
+                                        (show_emoji = false)
+                                "
+                            ></v-emoji>
+                        </div>
+                    </button>
+                    <button @click="insertNode('tag', 'tag', '[tag-value]')">
+                        tag
+                    </button>
+                    <button @click="insertNode('tag', 'no-value')">
+                        no-value
+                    </button>
+                </div>
+            </slot>
             <div
                 ref="textRef"
                 class="editor-content"
                 contenteditable="true"
-                v-html="content"
-                @input="input"
-                @click="getStart"
-                @blur="getStart"
+                v-html="value"
+                @input="getRange"
+                @click="getRange"
             ></div>
         </div>
     </div>
 </template>
 
 <script>
-import { reactive, ref, toRefs, watch } from "vue";
+import { reactive, ref, toRefs } from "vue";
 export default {
     name: "simple-editor",
-    components: {},
-    setup() {
+    props: {
+        value: {
+            type: [Number, String],
+            default: "",
+        },
+        text: {
+            type: [Number, String],
+            default: "",
+        },
+    },
+    emits: ["update:value", "update:text"],
+    setup(props, { emit }) {
         const state = reactive({
-            content: "",
-            index: 0, // 光标下标
+            show_emoji: false,
         });
+        const textRef = ref(null);
+        let range = null;
 
-        const add = (type, e) => {
-            const { text = "" } = state.form;
-            let str = "",
-                index = state.index;
+        // 插入节点
+        const insertNode = (type = "text", e, value = "") => {
+            textRef.value.focus();
+            const selection = getSelection();
+            selection.deleteFromDocument(); // 删除光标所选区域
+
+            if (!range) {
+                range = selection.getRangeAt(0);
+            }
+
+            let node = e;
             switch (type) {
-                case "new": {
-                    str = `<span class="tag">@进群新人</span>`;
+                case "text":
+                    node = document.createTextNode(e);
                     break;
-                }
-                case "emoji": {
-                    str = e;
+                case "tag":
+                    node = document.createElement("section");
+                    node.setAttribute("class", "tag");
+                    node.setAttribute("unselectable", "no");
+                    node.setAttribute("onmousedown", "return false");
+                    node.setAttribute("contenteditable", false); // 不可编辑
+                    node.innerHTML = e;
                     break;
-                }
-                case "all": {
-                    str = "@全员 ";
-                    // str = `<span class="tag">@全员</span>`;
+                default:
+                    node = e;
+                    break;
+            }
+
+            range.insertNode(node); // 插入节点
+            range.collapse(); //  true 折叠到 Range 的 start 节点，默认 false 折叠到 end 节点。
+            if (type !== "text") {
+                node.setAttribute("data-value", value);
+                range.insertNode(document.createElement("span"));
+                range.collapse(); //  true 折叠到 Range 的 start 节点，默认 false 折叠到 end 节点。
+            }
+            getRange();
+        };
+
+        // 获取光标
+        const getRange = () => {
+            const selection = getSelection();
+            range = selection.getRangeAt(0);
+            // console.log("selection=>", selection);
+            // console.log("range=>", range);
+
+            submit();
+        };
+
+        const submit = () => {
+            emit("update:value", getHtml());
+            const text = getText(textRef.value);
+            emit("update:text", text);
+            console.log("getText=>", text);
+        };
+        // 获取HTML
+        const getHtml = () => {
+            return textRef.value.innerHTML;
+        };
+
+        /**
+         * 获取文本
+         */
+        const getText = (node) => {
+            let str = "";
+            switch (node.nodeName) {
+                case "DIV":
+                    str = node.className !== "editor-content" ? "\n" : "";
+                    break;
+                case "#text":
+                    str = node.nodeValue;
+                    break;
+                case "SECTION": {
+                    const value = node.getAttribute("data-value");
+                    if (value) return value;
                     break;
                 }
                 default:
                     break;
             }
-            state.content = text.slice(0, index) + str + text.slice(index);
-        };
-
-        const textRef = ref(null);
-        const getStart = () => {
-            state.index = textRef.value.selectionStart;
-            console.log("state.index=>", state.index);
-        };
-
-        watch(
-            () => state.content,
-            (val = "", prev = "") => {
-                const diff = val.length - prev.length;
-                if (diff > 0) state.index += diff;
+            if (node.childNodes.length > 0) {
+                for (let el of node.childNodes) {
+                    str += getText(el);
+                }
             }
-        );
+            return str;
+        };
+
         return {
             ...toRefs(state),
             textRef,
-            input(e) {
-                console.log(123, e.target.innerHTML);
-            },
-            add,
-            getStart,
+            insertNode,
+            getRange,
+            getHtml,
+            getText,
         };
     },
 };
@@ -103,23 +179,28 @@ export default {
             height: 100%;
             outline: none;
 
-            // :deep(.new-people) {
-            //     display: inline-block;
-            //     height: 32px;
-            //     line-height: 32px;
-            //     padding: 0 15px;
-            //     background: @third-color;
-            //     border-radius: 4px;
-            //     color: #fd7b2a;
-            // }
+            :deep(.tag) {
+                display: inline-block;
+                height: 32px;
+                line-height: 32px;
+                padding: 0 15px;
+                background: #ffe5d3;
+                border-radius: 4px;
+                color: #fd7b2a;
+                margin: 2px 4px;
+            }
+        }
 
-            :deep(.ant-input) {
-                border: 0;
-                &:focus {
-                    border: 0;
-                    box-shadow: none;
-                }
-                outline: none;
+        .emoji-btn {
+            position: relative;
+
+            .emoji-warp {
+                position: absolute;
+                background-color: #fff;
+                border-radius: 2px;
+                box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);
+                left: 0;
+                top: 40px;
             }
         }
     }
